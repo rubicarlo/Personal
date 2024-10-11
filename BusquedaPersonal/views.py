f48f5aca1545c9c1ab24e8f2f4c899bb5957e361
rubicarlo <33545455+rubicarlo@users.noreply.github.com>
2024-10-11 14:19:26 +0200

    Foto base64

--- BusquedaPersonal/views.py
+++ BusquedaPersonal/views.py
@@ -1,10 +1,12 @@
-from django.http import  JsonResponse
+import base64
+from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, render
-from .models import PersonasModelo, VidaLaboral
 from django.db.models import Q
 from django.urls import reverse_lazy
 from django.views.generic import UpdateView, ListView
+from .models import PersonasModelo, VidaLaboral
 from .forms import EditarPersonaForm
+
 
 def personas_list(request):
     personas = PersonasModelo.objects.values('dni', 'nombre', 'apellidos').order_by('apellidos')
@@ -23,15 +25,27 @@ class EditarPersonaView(UpdateView):
     template_name = 'personapag.html'
     success_url = reverse_lazy('personas_list')
     pk_url_kwarg = 'dni'
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['persona'] = self.get_object()  # Asegura que 'persona' esté disponible
+        persona = self.get_object()
+        context['persona'] = persona
+
+        # Convertir la imagen a base64
+        if persona.foto:
+            
+            if persona.foto and isinstance(persona.foto, bytes):
+        
+                context['persona'].foto = f"data:image/jpeg;base64,{base64.b64encode(persona.foto).decode('utf-8')}"
+
         return context
+
     def form_valid(self, form):
         print("Formulario válido, guardando...")
         return super().form_valid(form)
+
     def form_invalid(self, form):
-        print("Errores en el formulario:", form.errors)  # Esto te ayudará a ver qué está fallando
+        print("Errores en el formulario:", form.errors)
         return super().form_invalid(form)
     
 class VidaLaboralView(ListView):
